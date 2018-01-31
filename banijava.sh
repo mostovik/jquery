e1bbbe1cc3651b02b7dcfadb117d4cec2cfe6016
root <root@box-codeanywhere.com>
2018-01-31 13:24:14 -0500

    Wed Jan 31 13:24:14 EST 2018

---
+++ banijava.sh
@@ -0,0 +1,4 @@
+sudo apt-get install software-properties-common -y
+sudo add-apt-repository ppa:webupd8team/java -y
+sudo apt-get install oracle-java8-installer -y
+sudo apt-get install oracle-java8-set-default
