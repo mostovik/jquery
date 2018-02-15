591587639d0e7bc03e60d4d7779eb9d2b9e8be87
root <root@box-codeanywhere.com>
2018-02-15 13:48:43 -0200

    git

--- a.cs
+++
@@ -1,7 +0,0 @@
-class HelloWorld{
-
-public static void Main(){
-       System.Console.WriteLine("HelloWorld in C#");
-}
-
-} //fim da classe HelloWorld
---
+++ a.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Document</title>
+<body>
+<?php
+for($i=0;$i<10;$i++)
+echo $i;
+?>
+
+</body>
